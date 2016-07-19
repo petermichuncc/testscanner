884ec19986d37aa00380eedc4161ca890116c660
petermichuncc <plarrimo@uncc.edu>
2016-07-19 17:35:58 -0400

    Added comments to the page that will show the pieces/bag and bags/box

--- client/templates/barcode/zero.js
+++ client/templates/barcode/zero.js
@@ -12,7 +12,25 @@ Meteor.subscribe('scans');
    
 //var nowsync=TimeSync.serverTime(null, 300000);
 Template.zero.helpers({
-    parts: function () {
+    piecesperbag: function () {
+
+    	/*
+			if the item id has 
+			31: box
+			37: label
+			33: 
+
+   	*/
+			/*
+					If there is a 31 in the front then it is a box
+
+		*/
+
+		/*
+			So I need to grab an object from the server that contains the 
+
+
+		*/
 
      
   }
---
+++ server/excel.js
@@ -0,0 +1,15 @@
+
+/*
+var toCSV = require('to-csv');
+ 
+var fileXLSX = "/server/test.xlsx";
+
+var fileCSV = "test.csv";
+ 
+toCSV.xlsxToCSV(fileXLSX, function (err, res) {
+  if (err) {
+    throw err;
+  }
+  console.log(res); // CSV text 
+});
+*/
