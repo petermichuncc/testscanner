8a4ca2da36e02e7b5f5cf232df7e0d98808b04e1
petermichuncc <plarrimo@uncc.edu>
2016-08-03 17:59:15 -0400

    Added a way to delete entries on history page.

--- client/templates/dataentry/dataentryhistory.js
+++ client/templates/dataentry/dataentryhistory.js
@@ -20,3 +20,39 @@ Template.history.helpers({
     }      
                                                              
 })
+Template.history.events({
+    'click tr':function(e, t){
+       
+var name=this.name
+var date=this.date
+var employeestatus=this.employeestatus
+var department=this.department
+var workcenter=this.workcenter
+var shift=this.shift
+var planned=this.planned
+var actual=this.actual
+var productivity=this.productivity
+var id=this._id
+console.log("You Select Client Row " + name);
+     console.log("You Select Client Row " + date);
+ console.log("You Select Client Row " + employeestatus);
+ console.log("You Select Client Row " + department);
+console.log("You Select Client Row " + workcenter);
+console.log("You Select Client Row " + shift);
+console.log("You Select Client Row " + planned);
+console.log("You Select Client Row " + actual);
+console.log("You Select Client Row " + productivity);
+console.log("You Select Client Row " + id);
+Session.set("idtest",id)
+//trigger a removal of this database entry when it is clicked
+
+Meteor.call('alertremoval', name,date,productivity,id)
+
+
+
+
+//
+        
+      
+    }
+});
