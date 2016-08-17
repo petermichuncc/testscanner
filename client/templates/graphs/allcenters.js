4c6481722cb6a82a097a1cc6e32aa02e660feb25
petermichuncc <plarrimo@uncc.edu>
2016-08-17 15:21:49 -0400

    Added bootstrap material design framework.

---
+++ client/templates/graphs/allcenters.js
@@ -0,0 +1,163 @@
+
+Meteor.subscribe('dataentries');
+ Meteor.subscribe('datacenters');
+Template.allworkcell.topGenresChart = function() {
+
+    //Here I need to have a server side fxn that  I call that returns
+    //the average for permanent and another for temp
+
+    console.log("This is the graph name " + Session.get("graphname"))
+    console.log("this is the workcenter name " + Session.get("workcenterName"))
+
+//based on the count I need to determine which data set to ship over to this graph
+//
+/*
+Basically I will 
+
+*/
+//I need to grab a average for every work center
+
+var both=ReactiveMethod.call('averageworkcenter',Session.get("workcenterName"))
+var shift1=both[0]
+var shift2=both[1]
+var shift3=both[2]
+
+
+if (typeof shift1=="number"||typeof shift2=="number"||typeof shift3=="number")
+{Session.set("loading",0)
+    var datatest=[
+                ['Shift1',  shift1],
+                ['Shift2',   shift2],
+                ['Shift3',   shift3]
+              
+               
+            ]
+    return {
+        chart: {
+            renderTo: 'container',
+            type: 'column',
+            options3d: {
+                enabled: true,
+                alpha: 0,
+                beta: 25,
+                depth: 32,
+                viewDistance: 40
+            },
+            style: {
+                    color: 'black',
+                    fontSize:'25px'
+                }
+        },
+        title: {
+            text: "Workcenter shift comparison"
+        },
+         xAxis: {
+            categories: ['Shift 1', 'Shift 2','Shift 3'],
+            labels: {
+                style: {
+                    color: 'black',
+                    fontSize:'25px'
+                }
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Productivity'
+            },
+            labels: {
+                 formatter: function () {
+                    return this.value + '%';
+                },
+                style: {
+                    color: 'black',
+                    fontSize:'25px'
+                }
+            },
+            stackLabels: {
+                enabled: true,
+                align: 'center',
+                formatter: function () {
+                    return this.y + '%';
+                }
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{this.y}</b> %',
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            },
+         series: {
+                dataLabels: {
+                    align: 'center',
+                    enabled: true,
+                    rotation: 0,
+                    x: 2,
+                    y: -10,
+                    formatter: function () {
+                       
+                            return this.y + '%';
+                       // return Highcharts.numberFormat(this.y,1);
+                   
+                }
+                },
+
+            }
+        },
+        series: [{
+            data: datatest
+        }]
+
+
+
+
+    };
+
+function showValues() {
+        $('#alpha-value').html(chart.options.chart.options3d.alpha);
+        $('#beta-value').html(chart.options.chart.options3d.beta);
+        $('#depth-value').html(chart.options.chart.options3d.depth);
+    }
+        showValues();
+        
+    }
+    else
+    {
+        Session.set("loading",1)
+    }
+};
+
+
+
+Template.allworkcell.rendered = function(){
+     
+
+
+};
+
+Template.allworkcell.helpers({
+    timeframe: function () {
+/*
+So here I need to show the user the time frame this is going on
+I need to grab the earliest entry timestamp so the first one
+and the last entry time stamp
+
+*/
+/*
+I'll need to contact a meteor method
+
+*/
+
+
+},
+workcenters: function()
+  {
+    //I will grab a session variable that has the 
+    var department =Session.get("department")
+    console.log("this is the department "+ department)
+    return Datacenters.find({department:department})
+
+  }
+})
--- server/meteormethods.js
+++ server/meteormethods.js
@@ -910,6 +910,99 @@ return avgarray
 
 
         },
+averageallworkcenter: function (workcenter) {
+       //So this function will take in an ekanban upc or item id
+       //It will then find the suffix of the item id and then find the associated
+       //color from the colors collection.    
+      try{
+
+        //This should take in a work center
+        console.log("this is the workcenter "+ workcenter)
+      /*  var string="/"
+        string=string.concat(workcenter)
+        string=string.concat("/")*/
+    
+        //count the amount of entries for each shift basically 
+        //I must do a regex for the workcenter
+      var shift1count=Dataentries.find({workcenter: { $regex: workcenter },shift:"shift 1"}).count()
+      var shift2count=Dataentries.find({workcenter: { $regex: workcenter },shift:"shift 2"}).count()
+      var shift3count=Dataentries.find({workcenter: { $regex: workcenter },shift:"shift 3"}).count()
+
+      console.log("this is the shift 1 count " + shift1count)
+    console.log("this is the shift 2 count " + shift2count)
+    console.log("this is the shift 3 count " + shift3count)
+
+    
+
+    
+       var totalshift1=0;
+        var totalshift2=0;
+         var totalshift3=0;
+
+       //var totalpermanent=0;
+       
+      var total = 0;
+
+Dataentries.find({workcenter: { $regex: workcenter },shift:"shift 1"}).map(function(doc) {
+  totalshift1 += doc.productivity;
+});
+Dataentries.find({workcenter: { $regex: workcenter },shift:"shift 2"}).map(function(doc) {
+  totalshift2 += doc.productivity;
+});
+
+console.log("this is the total shift 1 "+ totalshift1)
+if(totalshift1!=0 &&shift1count!=0)
+{
+var shift1avg=totalshift1/shift1count
+shift1avg=shift1avg.toFixed(2)
+shift1avg=Number(shift1avg)
+}
+else
+{
+  shift1avg=Number(0)
+}
+console.log("this is the shift1 avg " + shift1avg)
+if(totalshift2!=0 &&shift2count!=0)
+{
+var shift2avg=totalshift2/shift2count
+shift2avg=shift2avg.toFixed(2)
+shift2avg=Number(shift2avg)
+}
+else
+{
+  shift2avg=Number(0)
+}
+if(totalshift3!=0 &&shift3count!=0)
+{
+var shift3avg=totalshift3/shift3count
+shift3avg=shift3avg.toFixed(2)
+shift3avg=Number(shift3avg)
+}
+else
+{
+  shift3avg=Number(0)
+}
+
+//console.log("this is the temp total in base average" + totaltemp)
+
+var avgarray=[]
+avgarray.push(shift1avg)
+avgarray.push(shift2avg)
+avgarray.push(shift3avg)
+
+console.log("this is the array length "+ avgarray.length)
+console.log("this is array position 1 " + avgarray[0])
+return avgarray
+
+}catch(err)
+{
+  console.log("this is the error " + err)
+}
+
+
+        },
+
+
 average: function (department) {
        //So this function will take in an ekanban upc or item id
        //It will then find the suffix of the item id and then find the associated
